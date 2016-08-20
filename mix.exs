495802b425bebfb04aec3c28bec4d9d0a3fc17ba
Lenz Gschwendtner <lenz@springtimesoft.com>
2016-08-20 13:57:27 +1200

    added poison dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Venom.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :poison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Venom.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:poison, ">= 2.0.0"}]
   end
 end
